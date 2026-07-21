f034190cfcd8e63315ab62c9e9b57536c856e6b4
ThanushaEndrapalli <thanushathanusha17@gmail.com>
2026-07-21 20:34:45 +0530

    Time: 3 ms (8.79%), Space: 20.5 MB (75.21%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,5 +3,5 @@ class Solution:
         for i in range(len(nums)):
             if nums[i]==target:
                 return i
-            if target not in nums:
-                return -1    
+            # if target not in nums:
+        return -1    
